2edbb6b11245ae929ce751401b38bbcf7b440122
Jeff Epler <jepler@unpythonic.net>
2014-08-14 14:09:15 -0500

    Hard-code cpuid (CPU#) for rtai realtime
    this will help test a certain theory about isolcpus cpu numbering
    or maybe AMD bulldozer+ core/module sharing and realtime performance

--- src/rtapi/rtai_rtapi.c
+++ src/rtapi/rtai_rtapi.c
@@ -697,7 +697,8 @@ int rtapi_task_new(void (*taskcode) (void *), void *arg,
     task->arg = arg;
     /* call OS to initialize the task - use predetermined CPU */
     retval = rt_task_init_cpuid(ostask_array[task_id], wrapper, task_id,
-	 stacksize, prio, uses_fp, 0 /* signal */, rtapi_data->rt_cpu );
+	 stacksize, prio, uses_fp, 0 /* signal */,
+	 rtapi_data->rt_cpu > 2 ? 2 : rtapi_data->rt_cpu );
     if (retval != 0) {
 	/* couldn't create task, free task data memory */
 	kfree(ostask_array[task_id]);
